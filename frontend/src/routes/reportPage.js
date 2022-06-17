7eadad267840e1743115a67a30b491a3746d28d7
singhutkarshh <20bec116@iiitdmj.ac.in>
2022-06-17 16:51:32 +0530

    added  loader and alert

--- frontend/src/routes/reportPage.js
+++ frontend/src/routes/reportPage.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Axios from 'axios';
 import "../stylesheets/reportPage.css"
+import { Alert, CircularProgress } from '@mui/material';
 
 const ReportPage = () => {
+
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [isAlert, setIsAlert] = React.useState(false);
+
+    useEffect(() => {
+    }, [isLoading,isAlert])
+    
 
    const [postData, setPostData] = React.useState({firstName:"" , lastName:"", roll:"", phone:"",title:"", description:"",  location:"", type:"lost"});
    const [selectedFile, setSelectedFile] = React.useState("");  
@@ -14,7 +22,8 @@ const ReportPage = () => {
     formData.append('upload_preset', "vaogtmy6");
     let res,image="";
     try {
-      res = await Axios.post('https://api.cloudinary.com/v1_1/singhutkarshh/image/upload', formData);  
+        setIsLoading(true);
+        res = await Axios.post('https://api.cloudinary.com/v1_1/singhutkarshh/image/upload', formData);  
     } catch (error) {
         console.log(error);
         return;
@@ -33,6 +42,8 @@ const ReportPage = () => {
         data.image = image;
         let res = await Axios.post('http://localhost:8000/report/form',data);
         console.log(res.data);
+        setIsLoading(false);
+        setIsAlert(true);
     } 
     catch (error) {
             console.log(error);
@@ -42,82 +53,87 @@ const ReportPage = () => {
 
 
   return (
+    <>
+    {isAlert ? <Alert severity="success" onClose={() => {}}>Your Report was successfully Submitted!</Alert> : ""}
     <section className='reportForm'>
-    <div class="container">
-        <form action="#">
-            <div class="form first">
-                <div class="details personal">
-                    <span class="title">Personal Details</span>
-
-                    <div class="fields">
-                        <div class="input-field">
-                            <label>First Name</label>
-                            <input type="text" placeholder="Enter your first name" name='firstName' value={postData.firstName} onChange={(e)=>setPostData({...postData,firstName:e.target.value})} required />
-                        </div>
-
-                        <div class="input-field">
-                            <label>Last Name</label>
-                            <input type="text" placeholder="Enter your last name" name='lastname' value={postData.lastName} onChange={(e)=>setPostData({...postData,lastName:e.target.value})} required />
-                        </div>
-
-                        <div class="input-field">
-                            <label>Institute Roll Number</label>
-                            <input type="text" placeholder="Enter roll number" name='roll' value={postData.roll} onChange={(e)=>setPostData({...postData,roll:e.target.value})} required />
-                        </div>
-
-                        <div class="input-field">
-                            <label>Mobile Number</label>
-                            <input type="text" placeholder="Enter mobile number" name='phone' value={postData.phone} onChange={(e)=>setPostData({...postData,phone:e.target.value})} required />
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="details ID">
-                    <span class="title">Report Details</span>
-
-                    <div class="fields">
-                    <div class="input-field">
-                            <label>Report Type</label>
-                            <select name='type' defaultValue="lost" value={postData.type} onChange={(e)=>setPostData({...postData,type: e.target.value})} required>
-                                <option disabled  selected>Select Type</option>
-                                <option value="lost" >Lost</option>
-                                <option value="found">found</option>
-                            </select>
-                        </div>
-
-                        <div class="input-field">
-                            <label>Title</label>
-                            <input type="text" placeholder="eg: I lost my laptop charger  in cc" name='title' value={postData.title} onChange={(e)=>setPostData({...postData,title:e.target.value})} required />
-                        </div>
-                        <div class="input-field">
-                            <label>Location</label>
-                            <input type="text" placeholder="Enter last location you remember" name='location' value={postData.location} onChange={(e)=>setPostData({...postData,location:e.target.value})} required />
-                        </div>
-
-                        <div class="input-field" style={{width: "calc(2*100% / 3 + 15px)"}}>
-                            <label>Item Description</label>
-                            <textarea type="text" required rows={5} name='description' value={postData.description} onChange={(e)=>setPostData({...postData,description:e.target.value})}/>
-                        </div>
-                        <br />
-                        
-                    </div>
-                   <div  className='btnBox'>
-                         <div className='image-field'>
-                            <label >Upload Image</label>
-                            <input type='file' className='image-field'  name="image"  onChange={(e)=>setSelectedFile(e.target.files[0])} required />
-                        </div>
-                        <button class="nextBtn" onClick={(e)=>clickHandler(e)}>
-                            <span class="btnText">Next</span>
-                            <i class="uil uil-navigator"></i>
-                        </button>
-                    </div>
-                </div> 
-            </div>
-
-        </form>
-    </div>
-</section>
+    {isLoading ? <CircularProgress/> :(
+         <div class="container">
+         <form action="#">
+             <div class="form first">
+                 <div class="details personal">
+                     <span class="title">Personal Details</span>
+ 
+                     <div class="fields">
+                         <div class="input-field">
+                             <label>First Name</label>
+                             <input type="text" placeholder="Enter your first name" name='firstName' value={postData.firstName} onChange={(e)=>setPostData({...postData,firstName:e.target.value})} required />
+                         </div>
+ 
+                         <div class="input-field">
+                             <label>Last Name</label>
+                             <input type="text" placeholder="Enter your last name" name='lastname' value={postData.lastName} onChange={(e)=>setPostData({...postData,lastName:e.target.value})} required />
+                         </div>
+ 
+                         <div class="input-field">
+                             <label>Institute Roll Number</label>
+                             <input type="text" placeholder="Enter roll number" name='roll' value={postData.roll} onChange={(e)=>setPostData({...postData,roll:e.target.value})} required />
+                         </div>
+ 
+                         <div class="input-field">
+                             <label>Mobile Number</label>
+                             <input type="text" placeholder="Enter mobile number" name='phone' value={postData.phone} onChange={(e)=>setPostData({...postData,phone:e.target.value})} required />
+                         </div>
+ 
+                     </div>
+                 </div>
+ 
+                 <div class="details ID">
+                     <span class="title">Report Details</span>
+ 
+                     <div class="fields">
+                     <div class="input-field">
+                             <label>Report Type</label>
+                             <select name='type' defaultValue="lost" value={postData.type} onChange={(e)=>setPostData({...postData,type: e.target.value})} required>
+                                 <option disabled  selected>Select Type</option>
+                                 <option value="lost" >Lost</option>
+                                 <option value="found">found</option>
+                             </select>
+                         </div>
+ 
+                         <div class="input-field">
+                             <label>Title</label>
+                             <input type="text" placeholder="eg: I lost my laptop charger  in cc" name='title' value={postData.title} onChange={(e)=>setPostData({...postData,title:e.target.value})} required />
+                         </div>
+                         <div class="input-field">
+                             <label>Location</label>
+                             <input type="text" placeholder="Enter last location you remember" name='location' value={postData.location} onChange={(e)=>setPostData({...postData,location:e.target.value})} required />
+                         </div>
+ 
+                         <div class="input-field" style={{width: "calc(2*100% / 3 + 15px)"}}>
+                             <label>Item Description</label>
+                             <textarea type="text" required rows={5} name='description' value={postData.description} onChange={(e)=>setPostData({...postData,description:e.target.value})}/>
+                         </div>
+                         <br />
+                         
+                     </div>
+                    <div  className='btnBox'>
+                          <div className='image-field'>
+                             <label >Upload Image</label>
+                             <input type='file' className='image-field'  name="image"  onChange={(e)=>setSelectedFile(e.target.files[0])} required />
+                         </div>
+                         <button class="nextBtn" onClick={(e)=>clickHandler(e)}>
+                             <span class="btnText">Next</span>
+                             <i class="uil uil-navigator"></i>
+                         </button>
+                     </div>
+                 </div> 
+             </div>
+ 
+         </form>
+     </div>
+    )}
+    </section>
+    </>
   )
 }
 
