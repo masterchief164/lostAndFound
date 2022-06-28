fa9a9635a1c447ae98aac818a0cb6022833ec6ef
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-06-28 20:10:50 +0530

    refactor: restyle files to fix eslint new rules

--- frontend/src/Api/Data.js
+++ frontend/src/Api/Data.js
@@ -1,47 +1,49 @@
-import axios from "axios";
+import axios from 'axios';
 
 const fetchFound = async () => {
-    try {
-        const res = await axios.get("http://localhost:8000/found", { withCredentials: true });
-        if (res.status === 200) {
-            return res.data;
-        }
-    } catch (err) {
-        console.log(err);
-        return [];
+  try {
+    const res = await axios.get('http://localhost:8000/found', { withCredentials: true });
+    if (res.status === 200) {
+      return res.data;
     }
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
 };
 
 const fetchLost = async () => {
-    try {
-        const res = await axios.get("http://localhost:8000/lost", { withCredentials: true });
-        if (res.status === 200) {
-            return res.data;
-        }
-    } catch (err) {
-        console.log(err);
-        return [];
+  try {
+    const res = await axios.get('http://localhost:8000/lost', { withCredentials: true });
+    if (res.status === 200) {
+      return res.data;
     }
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
 };
 
 const sendAuthorizationCode = async (code, setUser) => {
-    try {
-        const res = await axios.post("http://localhost:8000/auth/googleLogin", { tokenId: code }, { withCredentials: true });
-        setUser(res.data.userData);
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const res = await axios.post('http://localhost:8000/auth/googleLogin', { tokenId: code }, { withCredentials: true });
+    setUser(res.data.userData);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const logout = async (setUser) => {
-    await axios.get("http://localhost:8000/auth/logout", { withCredentials: true })
-        .then((response) => {
-            if (response.status === 200) {
-                console.log("logged out");
-                localStorage.removeItem("userDataLost");
-                setUser(null);
-            }
-        });
+  await axios.get('http://localhost:8000/auth/logout', { withCredentials: true })
+    .then((response) => {
+      if (response.status === 200) {
+        console.log('logged out');
+        localStorage.removeItem('userDataLost');
+        setUser(null);
+      }
+    });
 };
 
-export { fetchFound, fetchLost, sendAuthorizationCode, logout };
+export {
+  fetchFound, fetchLost, sendAuthorizationCode, logout,
+};
