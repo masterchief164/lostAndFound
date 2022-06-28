b5162d160d47cc674a5bba861c6741aba68423d0
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-06-28 17:47:21 +0530

    Fix: no image issue

--- backend/controllers/report.controller.js
+++ backend/controllers/report.controller.js
@@ -1,16 +1,18 @@
 const { saveItem } = require('../services/report.service');
 const logger = require('../bin/winston.util');
-const { cloudinary } = require("../utils/cloudinary");
+const { cloudinary } = require('../utils/cloudinary');
 
 module.exports.createItem = async (req, res) => {
   try {
     const data = req.body;
     // console.log(data);
     const image = data.image;
-    // console.log(image);
-      const uploadedImage = await cloudinary.uploader.upload(image, { upload_preset: "lostAndFound" });
+    console.log(image);
+    if (image != null && image !== 'default') {
+      const uploadedImage = await cloudinary.uploader.upload(image, { upload_preset: 'lostAndFound' });
       console.log(uploadedImage.secure_url);
-    data.image = uploadedImage.secure_url;
+      data.image = uploadedImage.secure_url;
+    }
     const document = await saveItem(data);
     console.log(document);
     res.send(document);
