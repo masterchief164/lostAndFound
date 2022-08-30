9a7981b2010cbebe2375be76247751369a9aabfd
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-08-30 18:07:09 +0530

    feat: completed the email template

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 require('dotenv')
-  .config();
+    .config();
 
 const cors = require('cors');
 const express = require('express');
@@ -9,18 +9,82 @@ const app = express();
 const Router = require('./routes/index.router');
 
 app.use(cookieParser());
+app.set('view engine', 'ejs');
+
+const data = [{
+    "id": "62fa33a0cbc2396a690af096",
+    "description": "Finally we are free",
+    "image": "https://res.cloudinary.com/masterchief/image/upload/v1660564384/lostAndFound/iowv9ws2ubzcfjkulwjz.webp",
+    "location": "1800s",
+    "title": "independence",
+    "dateTime": "2022-08-15T11:52",
+    "itemTag": "free",
+    "claimedBy": "20bec116@iiitdmj.ac.in",
+    "type": "Found",
+    "firstName": "Shaswat",
+    "lastName": "Gupta",
+    "submittedBy": "20bec101@iiitdmj.ac.in"
+}, {
+    "id": "62e5ef2b023097ddff9d40f4",
+    "description": "Please claim this.",
+    "image": "default",
+    "location": "Hostel",
+    "title": "Pain",
+    "dateTime": "2022-07-31T02:54",
+    "itemTag": "feeling",
+    "claimedBy": "20bec101@iiitdmj.ac.in",
+    "type": "Found",
+    "firstName": "John",
+    "lastName": "Doe",
+    "submittedBy": "20bcs021@iiitdmj.ac.in"
+}, {
+    "id": "62d51bb6bf0242de17f593e6",
+    "description": "This is it!",
+    "image": "https://res.cloudinary.com/masterchief/image/upload/v1658133429/lostAndFound/gmelrurhnfzj1hqnxw51.png",
+    "location": "kfc",
+    "title": "bag",
+    "dateTime": "2022-07-16T08:36",
+    "itemTag": "bag",
+    "claimedBy": "20bcs125@iiitdmj.ac.in",
+    "type": "Found",
+    "firstName": "Utkarsh",
+    "lastName": "Singh",
+    "submittedBy": "20bec116@iiitdmj.ac.in"
+}, {
+    "id": "62d51db9bf0242de17f59401",
+    "description": "This is it!",
+    "image": "default",
+    "location": "lab",
+    "title": "card",
+    "dateTime": "2022-07-11T08:45",
+    "itemTag": "id card",
+    "claimedBy": "20bec101@iiitdmj.ac.in",
+    "type": "Found",
+    "firstName": "Utkarsh",
+    "lastName": "Singh",
+    "submittedBy": "20bec116@iiitdmj.ac.in"
+}]
+
+data.map(item => {
+    item.date = new Date(item.dateTime).toDateString();
+})
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.render('newsletter.ejs', {items: data});
+})
 
 app.use(express.urlencoded({
-  limit: '50mb',
-  extended: true,
+    limit: '50mb',
+    extended: true,
 }));
 
 app.enable('trust proxy');
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({limit: '50mb'}));
 app.use(cors({
-  credentials: true,
-  origin: ["http://localhost:3000","https://frontend-gamma-sage.vercel.app"]
+    credentials: true,
+    origin: ["http://localhost:3000", "https://frontend-gamma-sage.vercel.app"]
 }));
 
 app.use('/', Router);
