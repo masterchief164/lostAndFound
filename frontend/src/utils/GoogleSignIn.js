c86f4fd2c9732bdc1a2975c0105578662cb093cc
masterchief164 <rishugupta2001.rg@gmail.com>
2023-09-29 01:33:06 +0530

    feat: added Dockerfile and nginx configs

--- frontend/src/utils/GoogleSignIn.js
+++ frontend/src/utils/GoogleSignIn.js
@@ -2,6 +2,9 @@ const GoogleSignIn = (state) => {
   const loginUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
   const nonce = Math.floor(Math.random() * 100000);
 
+  console.log(import.meta.env.VITE_CLIENT_ID);
+  console.log(import.meta.env.VITE_REDIRECT_URL);
+
   const options = {
     client_id: import.meta.env.VITE_CLIENT_ID,
     redirect_uri: import.meta.env.VITE_REDIRECT_URL,
@@ -13,6 +16,8 @@ const GoogleSignIn = (state) => {
     scope: 'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
   };
   const qs = new URLSearchParams(options);
+
+  console.log(`${loginUrl}?${qs.toString()}`);
   return (`${loginUrl}?${qs.toString()}`);
 };
 
